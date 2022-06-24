64d8d0413ae670a3bb27f4ac02c76d51a73a1571
Jennifer Lane <jennifer.lane610@gmail.com>
2022-06-24 17:32:40 +0100

    Ocarina tested and created, with IPlay implementation

--- src/main/java/instruments/Ocarina.java
+++ src/main/java/instruments/Ocarina.java
@@ -1,4 +1,34 @@
 package instruments;
 
-public class Ocarina {
+import behaviours.IPlay;
+
+public class Ocarina extends Instrument implements IPlay {
+
+    private int numberOfHoles;
+    private String colour;
+    private String pitch;
+
+    public Ocarina(String material, String classification, int numberOfHoles, String colour, String pitch) {
+        super(material, classification);
+        this.numberOfHoles = numberOfHoles;
+        this.colour = colour;
+        this.pitch = pitch;
+    }
+
+    public int getNumberOfHoles() {
+        return this.numberOfHoles;
+    }
+
+    public String getColour() {
+        return this.colour;
+    }
+
+    public String getPitch() {
+        return this.pitch;
+    }
+
+    @Override
+    public String play() {
+        return "doo doo doo";
+    }
 }
--- src/test/java/OcarinaTest.java
+++ src/test/java/OcarinaTest.java
@@ -1,2 +1,46 @@
+import instruments.Ocarina;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class OcarinaTest {
+
+    private Ocarina ocarina;
+
+    @Before
+    public void before(){
+        ocarina = new Ocarina("ceramic", "wind", 6, "blue", "tenor");
+    }
+
+    @Test
+    public void hasMaterial(){
+        assertEquals("ceramic", ocarina.getMaterial());
+    }
+
+    @Test
+    public void hasClassification(){
+        assertEquals("wind", ocarina.getClassification());
+    }
+
+    @Test
+    public void hasNumberOfHoles(){
+        assertEquals(6, ocarina.getNumberOfHoles());
+    }
+
+    @Test
+    public void hasColour(){
+        assertEquals("blue", ocarina.getColour());
+    }
+
+    @Test
+    public void hasPitch(){
+        assertEquals("tenor", ocarina.getPitch());
+    }
+
+    @Test
+    public void canPlay(){
+        assertEquals("doo doo doo", ocarina.play());
+    }
 }
