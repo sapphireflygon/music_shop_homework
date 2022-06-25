2ebb9f6f980693ca43d8cfb5b28530d3f4ee2bfe
Jennifer Lane <jennifer.lane610@gmail.com>
2022-06-25 16:22:31 +0100

    Added glockenspiel for the lolz

---
+++ src/main/java/instruments/Glockenspiel.java
@@ -0,0 +1,8 @@
+package instruments;
+
+public class Glockenspiel extends Instrument {
+
+    public Glockenspiel(String material, String classification, double buyingPrice, double sellingPrice) {
+        super(material, classification, buyingPrice, sellingPrice);
+    }
+}
